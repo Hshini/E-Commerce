334dea59e9429826ba8e2dcf8a620466c377ffe2
Shini <halimshini@hotmail.com>
2023-03-23 00:01:15 -0400

    added database models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price:{
+      type:DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true,
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true, 
+    },
+    category_id:{
+      type:DataTypes.INTEGER,
+        references: {
+          model: "category",
+          key: "id",
+        },
+    },
   },
   {
     sequelize,
@@ -17,7 +45,7 @@ Product.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'product',
-  }
+  },
 );
 
 module.exports = Product;
